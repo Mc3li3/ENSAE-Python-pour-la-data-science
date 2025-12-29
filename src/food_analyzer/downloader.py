a26811b7a2bf6540201e288f0f8e17df8b7eea5e
aaronh42 <Aaron.haddad@ensae.fr>
2025-12-29 13:25:12 +0000

    test 2

--- src/food_analyzer/downloader.py
+++ src/food_analyzer/downloader.py
@@ -82,10 +82,10 @@ def downloader(saved_dataset):
 
     csv_path = os.path.abspath(csv_path)
 
-    if True or saved_dataset:
+    if IS_CI or saved_dataset:
         print("🤖 Utilisation du dataset ")
         df_final = pd.read_csv(csv_path)
-        if IS_CI:
+        if True:
             df_final = df_final.sample(n=100, random_state=42)
     else:
         print("💻 Mode Local : Chargement complet du Dataset.")
